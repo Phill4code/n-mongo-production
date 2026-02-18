4a601453331a2db754ec23a72dbfbb234be762b0
phill4code <phillipkawesa@gmail.com>
2026-02-19 00:56:04 +0300

    Created the songs schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/spotify-clone')],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+///6:25:25
